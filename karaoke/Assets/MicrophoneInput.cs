0d459a16b171b0c571d029c2cb9d404442355cee
David Whitney <david_whitney@brown.edu>
2016-02-06 15:00:49 -0500

    sound better

--- karaoke/Assets/MicrophoneInput.cs
+++ karaoke/Assets/MicrophoneInput.cs
@@ -3,15 +3,7 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class MicrophoneInput : MonoBehaviour {
 
-	public float sensitivity = 100;
-	public float loudness = 0;
-
 	void Start() {
-		//GetComponent<AudioSource>().clip = Microphone.Start(null, true, 10, 44100);
-		//GetComponent<AudioSource>().loop = false; // Set the AudioClip to loop
-		//GetComponent<AudioSource>().mute = false; 
-		//while (!(Microphone.GetPosition(AudioInputDevice) > 0)){} // Wait until the recording has started
-		//GetComponent<AudioSource>().Play(); // Play the audio source!
 
 		AudioSource aud = GetComponent<AudioSource>();
 		aud.clip = Microphone.Start("jam", true, 1, 44100);
@@ -21,19 +13,3 @@ public class MicrophoneInput : MonoBehaviour {
 		aud.Play();
 	}
 }
-
-//	void Update(){
-//		loudness = GetAveragedVolume() * sensitivity;
-//	}
-//
-//	float GetAveragedVolume()
-//	{ 
-//		float[] data = new float[256];
-//		float a = 0;
-//		GetComponent<AudioSource>().GetOutputData(data,0);
-//		foreach(float s in data)
-//		{
-//			a += Mathf.Abs(s);
-//		}
-//		return a/256;
-//	}
