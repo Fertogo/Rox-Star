51f6d650182dcf9665297a097017591dcd26655f
Fernando Trujano <trujano@mit.edu>
2016-02-06 20:56:08 -0500

    Try Sync

--- KaraokeKinect/Assets/NetworkCommunicator.cs
+++ KaraokeKinect/Assets/NetworkCommunicator.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class NetworkCommunicator : MonoBehaviour {
+public class NetworkCommunicator : NetworkBehaviour {
+	public Transform transform;
+	[SyncVar] Transform serverState;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		InitState();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Start () {
 		
 	}
+
+	void Update () {
+		if (isLocalPlayer) {			
+			CmdMove();
+		}
+		SyncState ();
+	}
+
+	[Command] void CmdMove() {
+		serverState = transform;
+	}
+
+	[Server] void InitState () {
+		serverState = transform;
+	}
+
+	void SyncState() {
+		this.transform = serverState;
+	}
+
 }
